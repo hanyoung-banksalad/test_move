b5ab3535fa48aa4e48605c7defe976401c5bc26f
Hanyoung Park <ha058@banksalad.com>
2022-08-08 17:47:23 +0900

    AA

--- server/handler/get_image_handler.go
+++ server/handler/get_image_handler.go
@@ -8,7 +8,7 @@ import (
 
 type GetImageHandlerFunc func(ctx context.Context, req *imageproxy.GetImageRequest) (*imageproxy.GetImageResponse, error)
 
-func GetImage() GetImgeHandlerFunc {
+func GetImage() GetImageHandlerFunc {
 	return func(ctx context.Context, req *imageproxy.GetImageRequest) (*imageproxy.GetImageResponse, error) {
 		return &imageproxy.GetImageResponse{
 			Path: "AA",
