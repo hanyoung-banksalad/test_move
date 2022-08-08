d71007045f97b57eff1f114abc7d6fc109857046
Hanyoung Park <ha058@banksalad.com>
2022-08-08 17:38:23 +0900

    aa

--- server/handler.go
+++ server/handler.go
@@ -3,8 +3,8 @@ package server
 import (
 	"context"
 
-	"github.com/banksalad/imageproxy/server/handler"
-	"github.com/banksalad/idl/gen/go/apis/v1/imageproxy"
+	"github.com/hanyoung-banksalad/imageproxy/server/handler"
+	"github.com/hanyoung-banksalad/idl/gen/go/apis/v1/imageproxy"
 )
 
 // verify ImageproxyServer implements all interface methods
@@ -12,4 +12,8 @@ var _ imageproxy.ImageproxyServer = (*ImageproxyServer)(nil)
 
 func (s *ImageproxyServer) HealthCheck(ctx context.Context, req *imageproxy.HealthCheckRequest) (*imageproxy.HealthCheckResponse, error) {
 	return handler.HealthCheck()(ctx, req)
+}
+
+func (s *ImageproxyServer) GetImage(ctx context.Context, req *imageproxy.GetImageRequest) (*imageproxy.GetImageResponse, error) {
+	return handler.GetImage()(ctx, req)
 }
