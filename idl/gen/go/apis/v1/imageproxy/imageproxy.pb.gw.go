51160ecc3c13691316b1ad7a58ad78723f3e07a7
Hanyoung Park <ha058@banksalad.com>
2022-08-17 13:13:25 +0900

    auto commit by  at

--- idl/gen/go/apis/v1/imageproxy/imageproxy.pb.gw.go
+++ idl/gen/go/apis/v1/imageproxy/imageproxy.pb.gw.go
@@ -49,6 +49,10 @@ func local_request_Imageproxy_HealthCheck_0(ctx context.Context, marshaler runti
 
 }
 
+var (
+	filter_Imageproxy_GetImage_0 = &utilities.DoubleArray{Encoding: map[string]int{"size": 0, "filename": 1}, Base: []int{1, 1, 2, 0, 0}, Check: []int{0, 1, 1, 2, 3}}
+)
+
 func request_Imageproxy_GetImage_0(ctx context.Context, marshaler runtime.Marshaler, client ImageproxyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetImageRequest
 	var metadata runtime.ServerMetadata
@@ -65,7 +69,7 @@ func request_Imageproxy_GetImage_0(ctx context.Context, marshaler runtime.Marsha
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "size")
 	}
 
-	protoReq.Size, err = runtime.Uint32(val)
+	protoReq.Size, err = runtime.Int32(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "size", err)
 	}
@@ -78,6 +82,13 @@ func request_Imageproxy_GetImage_0(ctx context.Context, marshaler runtime.Marsha
 	protoReq.Filename, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "filename", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Imageproxy_GetImage_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetImage(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -101,7 +112,7 @@ func local_request_Imageproxy_GetImage_0(ctx context.Context, marshaler runtime.
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "size")
 	}
 
-	protoReq.Size, err = runtime.Uint32(val)
+	protoReq.Size, err = runtime.Int32(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "size", err)
 	}
@@ -114,6 +125,13 @@ func local_request_Imageproxy_GetImage_0(ctx context.Context, marshaler runtime.
 	protoReq.Filename, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "filename", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Imageproxy_GetImage_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetImage(ctx, &protoReq)
